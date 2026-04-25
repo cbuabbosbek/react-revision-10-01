8e2063893011e0abfaaca0c1a73f0d8499c39ccf
Abbosbek <cbuabbosbek@gmail.com>
2026-04-25 18:05:55 +0500

    counter demo

--- src/pages/Counter.jsx
+++ src/pages/Counter.jsx
@@ -1,7 +1,37 @@
+import { useState } from "react";
+
 function CounterApp() {
+  const [count, setCount] = useState(0);
+
+  const plus = () => setCount((prev) => prev + 1);
+
+  const minus = () => {
+    if (count > 0) {
+      setCount((prev) => prev - 1);
+    }
+  };
   return (
     <>
-      <h1>Counter App</h1>
+      <div className="container wrap-counter">
+        <h1>
+          <i class="fa-solid fa-cart-shopping"></i>
+          <span className="counter-items">0</span> Items
+        </h1>
+        <div className="m-10">
+          <i class="fa-solid fa-arrows-rotate"></i>
+          <i class="fa-solid fa-recycle"></i>
+        </div>
+        <div>
+          <span className="count">{count}</span>
+          <button onClick={() => plus()} className="btn btn-start">
+            +
+          </button>
+          <button onClick={() => minus()} className="btn btn-stop">
+            -
+          </button>
+          <i class="fa-solid fa-trash"></i>
+        </div>
+      </div>
     </>
   );
 }
